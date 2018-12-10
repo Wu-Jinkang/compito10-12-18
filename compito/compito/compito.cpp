87330a221f377089c574d4fdf5e03f527f411a16
Wu-Jinkang <wujinkang2000@gmail.com>
2018-12-10 09:51:29 +0100

    Aggiunto altri commenti sul programma

--- compito/compito/compito.cpp
+++ compito/compito/compito.cpp
@@ -5,8 +5,8 @@ Wu Jinkang 5A Informatica 10/12/18
 */
 int main()
 {
-	int i = 1;
-	for (; i < 11; i++)
+	int i = 1; //Sono un commento
+	for (; i < 11; i++) //Anch'io
 		cout << i << endl;
 	system("PAUSE");
 	return 0;
